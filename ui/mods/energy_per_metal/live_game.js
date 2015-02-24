2f64275fe5704422743ba6286917cfc26d046e7a
Justin Love <git@JustinLove.name>
2015-02-24 05:40:24 -0600

    leave decimals for mods with closer ratios

--- ui/mods/energy_per_metal/live_game.js
+++ ui/mods/energy_per_metal/live_game.js
@@ -4,7 +4,7 @@
     vanillaUnitData(payload)
 
     _.forEach(payload.data, function (element, id) {
-        model.itemDetails[id].energyPerMetal = ko.observable(element.build_arm_cost ? Math.round(element.build_arm_cost / element.build_arm_power) : null);
+        model.itemDetails[id].energyPerMetal = ko.observable(element.build_arm_cost ? element.build_arm_cost / element.build_arm_power : null);
     });
   }
 })()
